67171a1b9a121723ffd114f3bfb0d5a99a6999b1
Duc Quan <leducquan16112002@gmail.com>
2024-10-25 16:04:43 +0700

    change

--- datasets/__init__.py
+++ datasets/__init__.py
@@ -24,6 +24,5 @@ def build_dataset(image_set, args):
         return build_coco_panoptic(image_set, args)
     
     if args.dataset_file == 'traffic-vehicle':
-        from .tvd import build as build_tvd
-        return build_tvd(image_set, args)
+        return build_coco(image_set, args)
     raise ValueError(f'dataset {args.dataset_file} not supported')
